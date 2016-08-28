73640010a1bcdde64801add452e118805b4117ee
jorcalle11 <jorcalle11@gmail.com>
2016-08-27 22:41:38 -0500

    fixed compile scss to css

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import concat from 'gulp-concat';
+import cleanCSS from 'gulp-clean-css';
 import paths from './paths';
 
 const options = {
@@ -11,5 +12,6 @@ export default () =>  {
   return gulp.src(paths.styles.scss)
     .pipe(sass(options).on('error', sass.logError))
     .pipe(concat('style.min.css'))
+    .pipe(cleanCSS())
     .pipe(gulp.dest(paths.styles.dir));
 };
